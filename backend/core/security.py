16fce8660e3ab2d0946e2947cf7d5eda55461865
g18oi <164824652+g18oi@users.noreply.github.com>
2025-12-21 21:07:05 +0900

    Add files via upload

---
+++ backend/core/security.py
@@ -0,0 +1,49 @@
+import hashlib
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+
+SECRET_KEY = "d3b4472f8f92a609c88e74d53603233e667163b14c355ba8503164e67c9f6089"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+def get_password_hash(password:str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password:str, hashed_password:str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data:dict, expires_delta: timedelta | None= None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(token:str = Depends(oauth2_scheme)):
+    from .. import models 
+    from .. import database
+    db = database.SessionLocal()
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="인증 정보가 유효하지 않습니다.",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if user is None:
+        raise credentials_exception
+    return user
